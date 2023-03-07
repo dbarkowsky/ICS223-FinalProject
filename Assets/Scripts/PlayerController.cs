4ea95a1dafc1269cbed0d92983f458a1dbfd79dd
Dylan Barkowsky <dylanbarkowsky@gmail.com>
2023-03-06 23:22:00 -0800

    added some shooting

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     private CharacterController cc;
     private float speed = 8f;
+    [SerializeField] GameObject bullet;
 
     // Start is called before the first frame update
     void Start()
@@ -18,8 +20,14 @@ public class PlayerController : MonoBehaviour
     {
         float xMove = Input.GetAxis("Horizontal");
         float yMove = Input.GetAxis("Vertical");
+        bool firing = Input.GetButton("Fire");
 
         Vector2 direction = new Vector2(xMove, yMove) * speed * Time.deltaTime;
         cc.Move(Vector2.ClampMagnitude(direction, 1));
+
+        if (firing)
+        {
+            Instantiate(bullet, transform);
+        }
     }
 }
